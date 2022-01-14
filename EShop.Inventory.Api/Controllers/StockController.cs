7e967407f248a3530af4588c8beec7d6de6b7305
Shashank Dhyani <shash.dhyani@gmail.com>
2022-01-14 12:25:02 +0530

    Episode - 60 - Validating Inventory API
    Add Stock Controller Actions methods.
    Renamed file to WalletController.

--- EShop.Inventory.Api/Controllers/StockController.cs
+++ EShop.Inventory.Api/Controllers/StockController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EShop.Infrastructure.Command.Inventory;
+using EShop.Inventory.DataProvider.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +13,24 @@ namespace EShop.Inventory.Api.Controllers
     [Route("[controller]/[action]")]
     public class StockController : ControllerBase
     {
-        public StockController()
+        private IInventoryService _inventoryService;
+        public StockController(IInventoryService inventoryService)
         {
+            _inventoryService = inventoryService;
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> AddStocks(AllocateProduct stock)
+        {
+            var isAdded = await _inventoryService.AddStocks(stock);
+            return Accepted(isAdded);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReleaseStocks(ReleaseProduct stock)
+        {
+            var isReleased = await _inventoryService.ReleaseStocks(stock);
+            return Accepted(isReleased);
         }
     }
 }
--- EShop.Order.Api/Handlers/CreateOrderHandler.cs
+++ EShop.Order.Api/Handlers/CreateOrderHandler.cs
@@ -1,8 +1,4 @@
 ﻿using MassTransit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace EShop.Order.Api.Handlers
 {
